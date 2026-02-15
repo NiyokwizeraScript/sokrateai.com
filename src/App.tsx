9bcc254d4e547a6a5d6e1a194e6a8b5344df95ef
NiyokwizeraScript <brillantniyokwizera@myyahoo.com>
2026-02-15 14:31:12 -0500

    Ajout Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { DashboardLayout } from "@/components/layout/DashboardLayout";
+import { Analytics } from "@vercel/analytics/react";
 
 // Pages
 import Landing from "@/pages/Landing";
@@ -55,6 +56,7 @@ function App() {
                     <Route path="*" element={<NotFound />} />
                 </Routes>
                 <Toaster />
+                <Analytics />
             </BrowserRouter>
         </QueryClientProvider>
     );
